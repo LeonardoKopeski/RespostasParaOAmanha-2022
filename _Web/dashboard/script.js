55159d79f619516dcdce66425cc8b3cb7c9b3013
= <leonardokop22@gmail.com>
2022-05-24 20:34:59 -0300

    Seleção de menus

--- _Web/dashboard/script.js
+++ _Web/dashboard/script.js
@@ -3,12 +3,19 @@ const socket = io()
 
 // Get Devices
 const cookie = getCookie("connections")
-if(cookie !== ""){
-    const cookieObj = JSON.parse(cookie)
-    cookieObj.forEach(elm => {
-        socket.emit("dashboard/getInfo", {fingerprint: elm})
-    })
+if(cookie === ""){
+    open("/linker","_SELF")
 }
-socket.on("dashboard/getInfo/response",(obj)=>{
-})
 
+// Set menu
+var thisMenu = "root"
+function setMenu(menu, name){
+    document.querySelector("div.main ."+thisMenu).classList.add("invisible")
+    document.querySelector("div.main ."+menu).classList.remove("invisible")
+
+    document.querySelector("div.main header h2").innerText = name
+
+    document.querySelector("div.main header svg").style.opacity = menu === "root"?0:1
+
+    thisMenu = menu
+}
