227900381f829ebd9e11a6539a91d1bb39a79285
= <leonardokop22@gmail.com>
2022-05-27 16:56:03 -0300

    Modelo dois para a dashboard

--- _Web/dashboard/script.js
+++ _Web/dashboard/script.js
@@ -1,21 +1,14 @@
 // Socket.io
 const socket = io()
 
-// Get Devices
-const cookie = getCookie("connections")
-if(cookie === ""){
-    open("/linker","_SELF")
-}
-
-// Set menu
-var thisMenu = "root"
-function setMenu(menu, name){
-    document.querySelector("div.main ."+thisMenu).classList.add("invisible")
-    document.querySelector("div.main ."+menu).classList.remove("invisible")
-
-    document.querySelector("div.main header h2").innerText = name
-
-    document.querySelector("div.main header svg").style.opacity = menu === "root"?0:1
-
-    thisMenu = menu
-}
+// Search Box
+var searchBox = document.querySelector("input.searchBox")
+searchBox.addEventListener("keyup", ()=>{
+    if(searchBox.value.length > 0){
+        document.querySelector(".left").classList.add("hidden")
+        document.querySelector(".right").classList.add("hidden")
+    }else{
+        document.querySelector(".left").classList.remove("hidden")
+        document.querySelector(".right").classList.remove("hidden")
+    }
+})
